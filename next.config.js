de884ab4024281158680c7eee0c7e460d6145347
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2025-03-24 23:24:40 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: 'out',
+    images: {
+        unoptimized: true,
+    },
+};
+
+module.exports = {
+    output: 'export',
+    distDir: 'out',
+};
